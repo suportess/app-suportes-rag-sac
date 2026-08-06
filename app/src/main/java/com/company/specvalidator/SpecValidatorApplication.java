bd3b435910700e323b29bc4d3dc0aef359bb9140
msimoes7 <msimoes7@its.jnj.com>
2026-08-06 15:20:23 -0300

    chore: trigger CI/CD

--- app/src/main/java/com/company/specvalidator/SpecValidatorApplication.java
+++ app/src/main/java/com/company/specvalidator/SpecValidatorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpecValidatorApplication {
+public class SpecValidatorApplication { // .
 
     public static void main(String[] args) {
         SpringApplication.run(SpecValidatorApplication.class, args);
